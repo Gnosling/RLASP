bae6d98deba57faf41688c5d162b63771cab335d
Gnosling <adrian.schmitt@freenet.de>
2021-04-24 16:01:14 +0200

    removed old frozenLakes-folder

--- src/mdp/frozenLakes/frozenLake.py
+++
@@ -1,8 +0,0 @@
-class FrozenLake:
-
-    def __init__(self, discount_rate, learning_rate, episodes, time_steps, gym):
-        self.disount_rate = discount_rate
-        self. learning_rate = learning_rate
-        self.episodes = episodes
-        self.time_steps = time_steps
-        self.gym = gym
